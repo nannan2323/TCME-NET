d1abdaeb947dac92cd9afb45ce8e857e7420daf9
nannan2323 <137466818+nannan2323@users.noreply.github.com>
2024-06-04 15:45:58 +0800

    Delete TCME-main directory

--- TCME-main/basicsr/Dataset.py
+++
@@ -1,2 +0,0 @@
-############BJ2-TC dataset download URL:
-# https://drive.google.com/file/d/1Bb5JygEjOH0r0DE-VwoUOEVjCzV1_vsF/view?usp=drive_link
--- TCME-main/basicsr/models/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-from .losses import (L1Loss, MSELoss, PSNRLoss, CharbonnierLoss, ColorAngleLoss, Edge_Loss)
-
-__all__ = [
-    'L1Loss', 'MSELoss', 'PSNRLoss', 'CharbonnierLoss', 'ColorAngleLoss', 'Edge_Loss'
-]
